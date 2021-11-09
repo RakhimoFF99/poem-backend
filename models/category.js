3bdfc1e4403cfd7b4e3171dc928ede396a7faa32
elyor <elyor3600610@gmail.com>
2021-11-09 16:12:43 +0500

    'sdsdsd

--- models/category.js
+++ models/category.js
@@ -14,16 +14,6 @@ const CategorySchema = new mongoose.Schema({
     timestamps: true
 })
 
-// Cascade delete category when a product is deleted
-CategorySchema.pre('remove', async function (next) {
-    // console.log(`Product being removed from category ${this._id}`);
-    await this.model('Product').deleteMany({
-        category: this._id
-    });
-    await this.model('Category').deleteMany({
-        parentId: this._id
-    });
-    next();
-});
+
 
 module.exports = mongoose.model('Category', CategorySchema)
